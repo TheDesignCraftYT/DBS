50193bf9646822ddc11079b4dc1623d88fa1ae02
TheDesignCraft <mail@thedesigncraft.de>
2022-11-21 18:00:40 +0100

    v1.0.0-alpha.1_2

--- src/test/java/de/thedesigncraft/discord/bot/stufftest/TestBot.java
+++ src/test/java/de/thedesigncraft/discord/bot/stufftest/TestBot.java
@@ -1,10 +1,13 @@
 package de.thedesigncraft.discord.bot.stufftest;
 
-public class TestBot {
+import de.thedesigncraft.discord.botstuff.essential.manage.Main;
+import de.thedesigncraft.discord.botstuff.essential.setup.StartupSetup;
+
+public class TestBot extends StartupSetup {
 
     public static void main(String[] args) {
 
-
+        new Main();
 
     }
 
