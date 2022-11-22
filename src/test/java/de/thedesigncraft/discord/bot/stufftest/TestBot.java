aa2ad9d2e01b1176171e154d874ab78e3363a540
TheDesignCraft <mail@thedesigncraft.de>
2022-11-22 13:31:44 +0100

    v1.0.0-alpha.1_3

--- src/test/java/de/thedesigncraft/discord/bot/stufftest/TestBot.java
+++ src/test/java/de/thedesigncraft/discord/bot/stufftest/TestBot.java
@@ -1,11 +1,22 @@
 package de.thedesigncraft.discord.bot.stufftest;
 
 import de.thedesigncraft.discord.botstuff.essential.manage.Main;
+import de.thedesigncraft.discord.botstuff.essential.manage.commands.discord.manage.categories.CommandCategoryBuilder;
+import de.thedesigncraft.discord.botstuff.essential.manage.versions.Version;
+import de.thedesigncraft.discord.botstuff.essential.manage.versions.VersionBuilder;
 import de.thedesigncraft.discord.botstuff.essential.setup.StartupSetup;
+
+import java.util.Collections;
 
 public class TestBot extends StartupSetup {
 
     public static void main(String[] args) {
+
+        setMainPackage("de.thedesigncraft.discord.bot.stufftest");
+        setProjectName("TestBot");
+        setVersions(Collections.singletonList(new VersionBuilder().setName("v1.0.0-alpha.1").setType(Version.Type.ALPHA).build()));
+        setCommandCategories(new CommandCategoryBuilder().setName("Test").build());
+        setToken(Version.Type.ALPHA, "MTA0MTAyNDM0MjY3NjQ4ODMwMw.GGYbcC.C0k_6cO01FQHTMrQlXUxYkB-UTyOuA97Yl2s_8");
 
         new Main();
 
